02277f9507fc8a63c50c43be308ba6fce94ef5d4
ashraf032 <101041982+ashraf032@users.noreply.github.com>
2022-03-25 19:29:01 +0400

    result commit

---
+++ 3dene4reqemli/3dene4reqemli/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _3dene4reqemli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3 dene 4 reqemli eded daxil edin ");
+
+            Console.WriteLine("I ededi daxil edin: ");
+            
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("II ededi daxil edin:");
+
+            string num2 = Console.ReadLine();
+
+
+            Console.WriteLine("III ededi daxil edin");
+
+            string num3 = Console.ReadLine();
+
+            int a = int.Parse(num1);
+            int b = int.Parse(num2);
+            int c = int.Parse(num3);
+
+            double d = a / 100 * 1;
+            double e = b / 100 * 2;
+            double f = c / 100 * 3;
+
+            double x = d - e - f;
+            double z = x + (c / 100 * 7);
+
+            if (a>=1000 && a<10000 || b >= 1000 && b < 10000 || c >= 1000 && c < 10000)
+            {
+                Console.WriteLine(z);
+            }
+            else
+            {
+                Console.WriteLine("Nezere alinmayib");
+            }
+        }
+    }
+}
